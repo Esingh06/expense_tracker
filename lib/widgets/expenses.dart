a3c3154006c4b50b7512df4eccd7f178f6112567
Eikamvir Singh <eikamvir10@gmail.com>
2026-03-31 11:29:50 -0400

    added in starting card and loaded in expense items

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,43 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget{
+  const Expenses ({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ExpensesState();
+  }
+
+}
+class _ExpensesState extends State<Expenses>{
+  final List<Expense> _registeredExpenses = [
+    Expense( 
+    title: "Gino's Pizza",
+    amount: 25.00,
+    date:DateTime.now(),
+    category: Category.food
+  ),
+  Expense( 
+    title: "Train Ticket",
+    amount: 15.25,
+    date:DateTime.now(),
+    category: Category.travel
+  )
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+         children: [
+            Text("Chart goes here"),
+            SizedBox(
+              height: 300,
+       child: ExpensesList(expenses : _registeredExpenses)),
+      ],
+      ),
+    );
+  }
+
+}
---
+++ lib/widgets/expenses_list/expenses_item.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+
+class ExpenseItem extends StatelessWidget{
+  const ExpenseItem ({super.key, required this.expense});
+  final Expense expense;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric
+        (horizontal: 20, 
+        vertical: 16),
+        child: Text(expense.title),
+      ),
+    );
+  }
+}
